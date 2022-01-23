4a4fc414e9879a2507fd593f067e71cc314f0206
Rafael-Wassoaski <meireles200@hotmail.com>
2022-01-22 21:03:16 -0300

    Corrigindo erro na criaçao de conversas e anexaçao de mensanges

--- src/routes/API/Conversas/Conversas.ts
+++ src/routes/API/Conversas/Conversas.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from 'express';
 import { Mensagens } from '../../../entities/Mensagem';
 import { Conversas, IConversa } from '../../../entities/Conversa';
-const ObjectId= require('mongoose').Types.ObjectId;
+import mongoose from 'mongoose';
 
 export async function listarConversas(req: Request, res: Response) {
 	const { user } = req.session;
 	const { userId } = req.params;
 	let conversation = await getConversation(user, userId);
-	
-	if (ObjectId.isValidObjectId(userId)) {
+
+	if (mongoose.isValidObjectId(userId)) {
 		if (!conversation) {
-			const users = [user.id, userId];
+			const users = [user._id, userId];
 			conversation = await Conversas.create({ usuarios: users, mensagens: [] });
 		}
 	}
@@ -23,24 +23,26 @@ function _getConversasByUserId(userId: String) {
 }
 
 async function getConversation(user1: string, user2: string): Promise<IConversa> {
-	return Conversas.findOne({ users: { $in: [user1, user2] } }).lean();
+	const conversation = await Conversas.findOne({ users: { $in: [user1, user2] } }).lean();
+	conversation.id = conversation._id || conversation.id;
+	
+	return conversation
 }
 
 export async function newMessage(req: Request, res: Response): Promise<Response> {
-	const { user: remetente } = req.session;
+	const { user } = req.session;
 	const { userId: destinatario } = req.params;
 	const { mensagem } = req.body;
-	
-	const conversation = await getConversation(remetente, destinatario);
-	
+	const conversation = await getConversation(user._id, destinatario);
+
 	if (conversation) {
-		const newMessage = await Mensagens.create({ remetente, destinatario, mensagem });
-		
-		await Conversas.updateOne({ _id: conversation.id }, { push: { mensagens: newMessage } });
-		
+		const newMessage = await Mensagens.create({ remetente: user._id, destinatario, mensagem });
+console.log(conversation);
+		const a = await Conversas.updateOne({ _id: conversation.id }, { $push: { mensagens: newMessage._id } });
+		console.log(a);
 		return res.json(newMessage);
 	}
-	
+
 	res.status(404);
 	return res.send();
 }
